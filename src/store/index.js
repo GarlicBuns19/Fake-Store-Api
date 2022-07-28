e4069a4ebe8ce5c86438cbfbdec5965c07ddcc8a
GarlicBuns19 <planterx26@gmail.com>
2022-07-28 06:05:41 +0100

    got shop data

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,18 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    products: ''
   },
   getters: {
   },
   mutations: {
   },
   actions: {
+    shopProducts: async (context) =>{
+      await fetch('https://fakestoreapi.com/products')
+      .then(res => res.json())
+      .then(data => context.state.products = data)
+    },
   },
   modules: {
   }
