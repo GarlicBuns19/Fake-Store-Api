cb35dbdf08f7c7357b062e98603d783dbd2f1128
GarlicBuns19 <planterx26@gmail.com>
2022-07-28 19:39:43 +0100

    changed names of files

---
+++ src/components/Landing.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Landing</h1>
+    <ul>
+        <li v-for="product of products" :key="product.id">
+            {{product.title}}
+        </li>
+    </ul>
+</template>
+<script>
+export default {
+    mounted(){
+        this.$store.dispatch('shopProducts')
+    },
+    computed:{
+        products(){
+            return this.$store.state.products
+        }
+    }
+}
+</script>
