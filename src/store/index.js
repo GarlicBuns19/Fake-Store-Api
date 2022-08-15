80c0adc87840a1610bcd2ef3bf26ea919c73721e
GarlicBuns19 <planterx26@gmail.com>
2022-08-15 20:25:33 +0100

    Sorting

--- src/store/index.js
+++ src/store/index.js
@@ -1,29 +1,40 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
-    products: '',
-    product: ''
+    products: "",
+    product: "",
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
-    stateProduct(state,payload){
-      state.product = payload
-    }
+    stateProduct(state, payload) {
+      state.product = payload;
+    },
+    sortByPrice(state){
+      state.products.sort((a, b) => a.price - b.price);
+    },
+    sortByPriceDesc(state){
+      state.products.sort((a, b) => b.price - a.price);
+    },
+    sortByTitle(state){
+      state.products.sort((a, b) => {
+        if(a.title < b.title)return -1;{
+          return 1
+        }
+      });
+    },
   },
   actions: {
-    shopProducts: async (context) =>{
-      await fetch('https://fakestoreapi.com/products')
-      .then(res => res.json())
-      .then(data => context.state.products = data)
+    shopProducts: async (context) => {
+      await fetch("https://fakestoreapi.com/products")
+        .then((res) => res.json())
+        .then((data) => (context.state.products = data));
     },
-    getProduct: async (context, id) =>{
+    getProduct: async (context, id) => {
       await fetch(`https://fakestoreapi.com/products/${id}`)
-      .then(res => res.json())
-      .then(data => context.commit('stateProduct', data))
+        .then((res) => res.json())
+        .then((data) => context.commit("stateProduct", data));
     },
   },
-  modules: {  
-  }
-})
+  modules: {},
+});
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -3,11 +3,19 @@
   <h2>We have what you want</h2>
   <div class="container" v-if="products">
     <div class="row">
+        <div class="col-md-6">
+            <button @click="sortByPrice">Sort by Price</button>
+            <button @click="sortByPriceDesc">Sort by Price</button>
+            <button @click="sortByTitle">Sort by title</button>
+        </div>
+        <div class="col-md-6">
+        </div>
       <div
         class="col-12 col-md-3 col-lg-3"
         v-for="product in products"
         :key="product.id"
       >
+        <h1>R {{product.price}} </h1>
         <h3>{{ product.id }}</h3>
         {{ product.title }}
         <router-link :to="{name : 'product', params : {id : product.id}  }">
@@ -22,6 +30,17 @@
 export default {
   mounted() {
     this.$store.dispatch("shopProducts");
+  },
+  methods:{
+    sortByPrice(){
+        this.$store.commit("sortByPrice");
+    },
+    sortByPriceDesc(){
+        this.$store.commit("sortByPriceDesc");
+    },
+    sortByTitle(){
+        this.$store.commit("sortByTitle");
+    }
   },
   computed: {
     products() {
